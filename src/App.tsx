8b6dd3331747c62dd2d0d912f57bb5436a6760e0
Sergey-Sh <sergeihar82@gmail.com>
2022-01-23 13:02:04 +0300

    Trial version of usage TypeScript and Redux-Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAppSelector, useAppDispatch } from "./hooks";
+import { selectCount, increment, decrement } from "./slice/counterSlice";
+import "./App.css";
 
 function App() {
+  const number = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch(decrement())}> - </button>
+      {number}
+      <button onClick={() => dispatch(increment())}> + </button>
     </div>
   );
 }
